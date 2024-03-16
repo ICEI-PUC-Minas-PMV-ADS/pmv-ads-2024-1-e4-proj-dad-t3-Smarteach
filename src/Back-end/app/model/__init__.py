a732eca20b143b67600919a77fcf61917179031e
Joao Pedro Nonato Santos <jpnonato@yahoo.com.br>
2024-03-16 13:50:23 -0300

    adjust model files

--- src/Back-end/app/model/__init__.py
+++ src/Back-end/app/model/__init__.py
@@ -1,47 +1,7 @@
-from datetime import datetime
+from .Teacher import Teacher
+from .Student import Student
+from .Content import Content
+from .Admin import Admin
 
 
-class Teacher():
-    def __init__(self, **kwargs):
-        self.nome = kwargs['nome']
-        self.disciplina = kwargs['disciplina']
-        self.turmas = kwargs['turmas']
-        self.turno = kwargs['turno']
-        self.data_cadastro = datetime.now().strftime("%d/%m/%Y - %H:%M")
-        self.data_atualizacao = datetime.now().strftime("%d/%m/%Y - %H:%M") 
-    
 
-    @staticmethod
-    def verify_new_teacher_data(data: dict):
-
-        available_keys = ['nome', 'disciplina', 'turmas', 'turno']
-
-        data_keys = data.keys()
-
-        if len(data_keys) != 4:
-            return f'há campos faltantes no corpo da requisição.'
-
-        wrong_keys = [key for key in data_keys if key not in available_keys]
-
-        if wrong_keys:
-            return f'Campos incorretos inseridos na requisição: {wrong_keys}'
-        
-        none_values = {key for key in data if not data[key] }
-
-        if none_values:
-            return f'Faltou atribuir os valores das seguintes propriedades: {none_values}'
-        
-        wrong_values = none_values = {key for key in data if type(data[key]) is not str and type(data[key]) is not list }
-
-        if wrong_values:
-            return f'Valores incorretos foram atribuidos as seguintes propriedades: {none_values}'
-
-class Student():
-    pass
-
-
-class Admin():
-    pass
-
-class Content():
-    pass
