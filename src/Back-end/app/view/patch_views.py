17aadbc3d874f4d93372e03fd46c6325d026136d
felipesobrinho <felipe.sobrinho.3@outlook.com>
2024-03-22 15:13:30 -0300

    :sparkles: create classes delete e patch route

--- src/Back-end/app/view/patch_views.py
+++ src/Back-end/app/view/patch_views.py
@@ -1,9 +1,17 @@
 from flask import request
 
 from app.controller import update_teacher_profile
+from app.controller import update_class
 
 def patch_routes(app):
     @app.patch('/teacher')
     def change_teacher_data_profile():
         data = request.get_json()
         return update_teacher_profile(data)
+    
+    @app.patch('/class')
+    def change_class_data():
+        data = request.get_json()
+        return update_class(data)
+    
+
