649a390f8745109f2375d382f38bb5e8d47200b8
felipesobrinho <felipe.sobrinho.3@outlook.com>
2024-05-05 12:59:33 -0300

    :recycle: fix: erros console e imagens alt

--- src/Front-end-web/src/app/page.jsx
+++ src/Front-end-web/src/app/page.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Home() { 
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center gap-10">
+    <div className="container flex flex-col justify-center items-center gap-10 mb-6">
       <Link href="/cadastro/turma"> <Button> Cadastrar Nova Turma </Button> </Link>
       <div className="flex">
         <ClassList />
