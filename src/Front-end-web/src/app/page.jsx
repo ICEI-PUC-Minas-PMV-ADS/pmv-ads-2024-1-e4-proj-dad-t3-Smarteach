554e7b29e1dad615d0e530a70c58ab65c852b50f
Thais Menezes <thais.mas@hotmail.com>
2024-05-05 09:31:17 -0300

    Adicionando tela de turmas e alterando header

--- src/Front-end-web/src/app/page.jsx
+++ src/Front-end-web/src/app/page.jsx
@@ -8,7 +8,6 @@ export default function Home() {
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-10">
-      <Link href="/cadastro/turma"> <Button> Cadastrar Nova Turma </Button> </Link>
       <div className="flex">
         <ClassList />
         <Mural />
--- src/Front-end-web/src/components/Header/index.jsx
+++ src/Front-end-web/src/components/Header/index.jsx
@@ -1,28 +1,46 @@
-'use client'
+"use client";
 import Title from "../logo";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import Link from "next/link"
+import Link from "next/link";
 import { CircleUser, GraduationCap, LogOut } from "lucide-react";
 import { useSession } from "next-auth/react";
 import ButtonLogout from "../logout-button";
+import { useState } from "react";
 
 export default function Header() {
- const session = useSession();
-
- return (
-  <>
-    {session.status == "authenticated" && (
+  const session = useSession();
+  const [isHoverCap, setIsHoverCap] = useState(false);
+  const [isHoverUser, setIsHoverUser] = useState(false);
+  return (
+    <>
+      {session.status == "authenticated" && (
         <div className="w-full h-[60px] flex items-center justify-center mb-12 pt-5">
           <div className="w-[1280px] flex items-center justify-between">
-            <Link href="/"><Title /></Link>
+            <Link href="/">
+              <Title />
+            </Link>
             <div className="flex items-center gap-10">
               <div className="flex items-center justify-center gap-4">
-                {session?.data?.user?.role === "admin" && 
-                  <div className="flex gap-4"> 
-                    <Link href="/usuarios"> <CircleUser/> </Link>
-                    <Link href="/"> <GraduationCap/> </Link>
+                {session?.data?.user?.role === "admin" && (
+                  <div className="flex gap-4">
+                    <Link
+                      href="/usuarios"
+                      onMouseEnter={() => setIsHoverUser(true)}
+                      onMouseLeave={() => setIsHoverUser(false)}
+                    >
+                      {" "}
+                      <CircleUser />{" "}
+                    </Link>
+                    <Link
+                      href="/turmas"
+                      onMouseEnter={() => setIsHoverCap(true)}
+                      onMouseLeave={() => setIsHoverCap(false)}
+                    >
+                      {" "}
+                      <GraduationCap />{" "}
+                    </Link>
                   </div>
-                }
+                )}
                 <ButtonLogout />
               </div>
               <div className="flex items-center justify-center gap-3">
@@ -32,11 +50,16 @@ export default function Header() {
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
               </div>
+              {isHoverUser && (
+                <div className="absolute text-xs mt-10 mr-10">Usuários</div>
+              )}
+              {isHoverCap && (
+                <div className="absolute text-xs mt-10 ml-8">Turmas</div>
+              )}
             </div>
           </div>
         </div>
-      )
-    }
-  </>
- );
+      )}
+    </>
+  );
 }
