554f4e84113217fad9d5a42043acceb10981663a
felipesobrinho <felipe.sobrinho.3@outlook.com>
2024-05-05 21:55:17 -0300

    :recycle: refact: detalhes perfil

--- src/Front-end-web/src/app/page.jsx
+++ src/Front-end-web/src/app/page.jsx
@@ -10,14 +10,14 @@ const session = useSession();
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-10">
       {session?.data?.user?.role === "admin" && 
-    <div className="container flex flex-col justify-center items-center gap-10 mb-6">
-      <Link href="/cadastro/turma"> <Button> Cadastrar Nova Turma </Button> </Link>
-      </div>
+        <div className="container flex flex-col justify-center items-center gap-10 mb-6">
+          <Link href="/cadastro/turma"> <Button> Cadastrar Nova Turma </Button> </Link>
+        </div>
       }
-      <div className="flex">
-        <ClassList />
-        <Mural />
-      </div>
+        <div className="flex">
+          <ClassList />
+          <Mural />
+        </div>
     </div>
   );
 }
