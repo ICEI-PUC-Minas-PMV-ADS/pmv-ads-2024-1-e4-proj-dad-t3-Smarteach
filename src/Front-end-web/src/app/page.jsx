a5e38ab8b36e8f240f5b94d793457199e6d88baa
Geovanne <114536934+Geovanneh@users.noreply.github.com>
2024-05-19 02:40:29 -0300

    Reposicionamento botão CADASTRAR NOVA TURMA

--- src/Front-end-web/src/app/page.jsx
+++ src/Front-end-web/src/app/page.jsx
@@ -6,13 +6,8 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Home() { 
-  const session = useSession();
-
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-10 mb-10">
-      {session?.data?.user?.role === "admin" && 
-        <Link href="/cadastro/turma"> <Button> Cadastrar Nova Turma </Button> </Link>
-      }
       <div className="flex">
         <ClassList />
         <Mural />
--- src/Front-end-web/src/components/mural/index.jsx
+++ src/Front-end-web/src/components/mural/index.jsx
@@ -1,25 +1,45 @@
 import Link from "next/link";
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
 
 const MuralHome = () => {
-    return (
-        <div className="flex flex-col w-full justify-center items-center h-full min-w-[300px] gap-10">
-              <div className="flex flex-col justify-center items-center">
-                <h2 className="text-3xl pb-5 text-primaryColor font-bold"> Mural </h2>
-                
-                <Link href="/">
-                  <Image width={300} alt="imagem" height={300} src={"https://img.freepik.com/vetores-gratis/formulas-cientificas-na-lousa_23-2148494010.jpg"} className="bg-black shadow-2xl rounded-2xl w-[250px] h-[100px] object-cover"/>
-                </Link>
-              </div>
-              <div className="flex flex-col justify-center items-center">
-                <h2 className="text-3xl pb-5 text-primaryColor font-bold"> Cronograma </h2>
-                <Link href="/">
-                  <Image width={300} alt="imagem" height={300} src={"https://img.freepik.com/vetores-gratis/formulas-cientificas-na-lousa_23-2148494010.jpg"} className="bg-black shadow-2xl rounded-2xl w-[250px] h-[100px] object-cover"/>
-                </Link>
-              </div>
-        </div>
-    )
-}
+  const { data: session } = useSession();
+
+  return (
+    <div className="flex flex-col w-full justify-center items-center h-full min-w-[300px] gap-10">
+      {session?.user?.role === "admin" && (
+        <Link href="/cadastro/turma">
+          <Button> Cadastrar Nova Turma </Button>
+        </Link>
+      )}
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-3xl pb-5 text-primaryColor font-bold"> Mural </h2>
+        <Link href="/">
+          <Image
+            width={300}
+            alt="imagem"
+            height={300}
+            src={"https://img.freepik.com/vetores-gratis/formulas-cientificas-na-lousa_23-2148494010.jpg"}
+            className="bg-black shadow-2xl rounded-2xl w-[250px] h-[100px] object-cover"
+          />
+        </Link>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-3xl pb-5 text-primaryColor font-bold"> Cronograma </h2>
+        <Link href="/">
+          <Image
+            width={300}
+            alt="imagem"
+            height={300}
+            src={"https://img.freepik.com/vetores-gratis/formulas-cientificas-na-lousa_23-2148494010.jpg"}
+            className="bg-black shadow-2xl rounded-2xl w-[250px] h-[100px] object-cover"
+          />
+        </Link>
+        
+      </div>
+    </div>
+  );
+};
 
 export default MuralHome;
