3603bb34c7f0ea720d01aa2d588e43d51ee030fe
Geovanne <114536934+Geovanneh@users.noreply.github.com>
2024-05-24 21:11:55 -0300

    Estilização final Mural e Cronograma

--- src/Front-end-web/src/app/detalhes/turma/[id]/cronograma/page.jsx
+++ src/Front-end-web/src/app/detalhes/turma/[id]/cronograma/page.jsx
@@ -6,11 +6,26 @@ import SecondaryButton from "@/components/secondary-button";
 import { obterTarefasDoDia } from "@/utils/obterTarefasDoDia";
 import Link from "next/link";
 
-const Cronograma = ({params}) => {
-    const [ date, setDate ] = useState(new Date());
-    const {classProfileData} = getClassProfile(params.id)
+const Cronograma = ({ params }) => {
+    const [date, setDate] = useState(new Date());
+    const { classProfileData } = getClassProfile(params.id)
 
     const tarefas = obterTarefasDoDia(classProfileData, date);
+    const leiaMais = (id) => {
+        const pontos = document.getElementById(`pontos-${id}`);
+        const maisTexto = document.getElementById(`mais-${id}`);
+        const btnLeiaMais = document.getElementById(`btnLeiaMais-${id}`);
+
+        if (pontos.style.display === "none") {
+            pontos.style.display = "inline";
+            maisTexto.style.display = "none";
+            btnLeiaMais.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500 mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M19 9l-7 7-7-7"/></svg>';
+        } else {
+            pontos.style.display = "none";
+            maisTexto.style.display = "inline";
+            btnLeiaMais.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500 mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 15l7-7 7 7"/></svg>';
+        }
+    }
 
     return (
         <div className="container flex flex-col justify-center items-center gap-5">
@@ -19,10 +34,10 @@ const Cronograma = ({params}) => {
             </div>
             <div className="flex w-full gap-5">
                 <SecondaryButton label={"Filtrar Aulas"} />
-                <Link href="/cadastro/tarefas"> <SecondaryButton label={"Adicionar Aulas"}/> </Link>
+                <Link href="/cadastro/tarefas"> <SecondaryButton label={"Adicionar Aulas"} /> </Link>
             </div>
             <div className="flex flex-col justify-center items-center gap-10">
-                <Calendar 
+                <Calendar
                     mode="single"
                     selected={date}
                     onSelect={setDate}
@@ -32,11 +47,29 @@ const Cronograma = ({params}) => {
                     <div className="flex flex-col justify-center items-center gap-5">
                         <h2 className="text-2xl font-bold text-primaryColor">Tarefas:</h2>
                         {tarefas.map((tarefa, index) => (
-                            <div key={index}>
-                                <p>Data: {tarefa.dia}/{tarefa.mes}/{tarefa.ano} - Horário: {tarefa.hora} </p>
-                                <p>Nome: {tarefa.name}</p>
-                                <p>Matéria: {tarefa.subject}</p>
-                                <p>Email do Professor: {tarefa.teacher_email}</p>
+                            <div className="bg-gray-200 rounded-xl" key={index}>
+                                <div className="flex bg-primaryColor rounded-xl justify-between px-8">
+                                    <p className="text-2xl font-bold text-white mt-3 ml-7">{tarefa.name}</p>
+                                    <p className="flex text-m text-white mt-2 mr-7">{tarefa.dia}/{tarefa.mes} <br /> {tarefa.hora}</p>
+                                </div>
+                                <div>
+                                    <p className="flex text-2xl font-light text justify-center mt-3 text-gray-500">{tarefa.subject}</p>
+                                </div>
+                                <div className="flex justify-center">
+                                    <img className="flex rounded-xl justify-center items-center mt-5 mb-3 h-[280px]"
+                                        src="https://img.global.news.samsung.com/br/wp-content/uploads/2018/01/flip-samsung.png" alt="Imagem de exemplo"></img>
+                                </div>
+                                <div className="container">
+                                    <p className=" text-x leading-5 text-gray-500 mt-3 mb-3 px-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
+                                        Nam semper efficitur orci ac pellentesque. Nullam luctus euismod eros.<br />
+                                        Duis mollis, ante in mollis elementum, sapien elit congue felis, nec commodo<br />
+                                        velit risus sit amet quam. Quisque elementum orci non sagittis commodo.<br />
+                                        In egestas ligula turpis, id scelerisque arcu pharetra et.<br />
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /></p>
+                                    <p className="text-x leading-5 text-gray-500 mt-3 mb-3 px-10"><strong>Email:</strong> {tarefa.teacher_email}</p>
+                                </div>
+
+
                             </div>
                         ))}
                     </div>
@@ -46,9 +79,11 @@ const Cronograma = ({params}) => {
                         <h2 className="text-2xl font-bold text-primaryColor">Não há tarefas para o dia selecionado</h2>
                     </div>
                 )}
-            </div>        
+            </div>
+
         </div>
-    )
+    );
+
 }
 
 export default Cronograma;
--- src/Front-end-web/src/app/detalhes/turma/[id]/mural/page.jsx
+++ src/Front-end-web/src/app/detalhes/turma/[id]/mural/page.jsx
@@ -22,71 +22,70 @@ const Mural = ({ params }) => {
   }
 
   return (
-    <div className="container flex flex-col justify-center items-center">
-      <div className="w-full bg-primaryColor p-5 rounded-xl flex justify-between items-center">
+    <div className="container">
+      <div className="bg-primaryColor p-4 rounded-xl">
         <h1 className="text-3xl font-bold text-white">Turma {classProfileData?.number}</h1>
       </div>
-
-      {classProfileData?.timeline && Object.keys(classProfileData.timeline).map(year => {
-        const months = Object.keys(classProfileData.timeline[year]);
-        if (months.length === 0) return null;
-        return (
-          <div key={year} className=" flex-row mt-4 mb-10">
-            {months.map(month => {
-              const days = Object.keys(classProfileData.timeline[year][month]);
-              if (days.length === 0) return null;
-              return (
-                <div key={month}>
-                  {days.map(day => {
-                    const hours = Object.keys(classProfileData.timeline[year][month][day]);
-                    if (hours.length === 0) return null;
-                    return (
-                      <div key={day}>
-                        {hours.map(hour => {
-                          const task = classProfileData.timeline[year][month][day][hour];
-                          if (!task) return null;
-                          const taskId = `${year}-${month}-${day}-${hour}`;
-                          return (
-                            <div key={hour} className="flex flex-row bg-gray-200 rounded-xl shadow-xl mt-7">
-                              <ul className="flex flex-col">
-
+      <div className=" h-[1000px] flex">
+        {classProfileData?.timeline && Object.keys(classProfileData.timeline).map(year => {
+          const months = Object.keys(classProfileData.timeline[year]);
+          if (months.length === 0) return null;
+          return (
+            <div key={year} className="flex justify-center container">
+              {months.map(month => {
+                const days = Object.keys(classProfileData.timeline[year][month]);
+                if (days.length === 0) return null;
+                return (
+                  <div key={month}>
+                    {days.map(day => {
+                      const hours = Object.keys(classProfileData.timeline[year][month][day]);
+                      if (hours.length === 0) return null;
+                      return (
+                        <div key={day}>
+                          {hours.map(hour => {
+                            const task = classProfileData.timeline[year][month][day][hour];
+                            if (!task) return null;
+                            const taskId = `${year}-${month}-${day}-${hour}`;
+                            return (
+                              <div key={hour} className="bg-gray-200 rounded-xl shadow-xl mt-5 mr-5 p-5">
                                 <div className="flex bg-primaryColor rounded-xl justify-between px-8">
-                                  <p className="text-2xl font-bold text-white mt-3 ml-7">{task.name}</p>
-                                  <p className="flex text-m text-white mt-2 mr-7">{day}/{month} <br /> {hour}</p>
+                                  <p className="text-2xl font-bold text-white mt-3">{task.name}</p>
+                                  <p className="flex text-m text-white mt-2">{day}/{month} <br /> {hour}</p>
                                 </div>
-
                                 <div>
                                   <p className="flex text-2xl font-light text justify-center mt-3 text-gray-500">{task.subject}</p>
                                   <p className=""><span id={`pontos-${taskId}`}></span>
                                     <span id={`mais-${taskId}`} style={{ display: 'none' }}>
-                                      <p className="text-x leading-5 text-gray-500 mt-3 mb-3 px-10"><strong>Email:</strong> {task.teacher_email}</p></span>
+                                      <p className="text-x leading-5 text-gray-500 mt-3 mb-3 px-10"><strong>Email:</strong> {task.teacher_email}</p>
+                                      <p className=" text-x leading-5 text-gray-500 mt-3 mb-3 px-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed sollicitudin ex.<br />
+                                        Nam semper efficitur orci ac pellentesque. Nullam luctus euismod eros.<br />
+                                        Duis mollis, ante in mollis elementum, sapien elit congue felis, nec commodo<br />
+                                        velit risus sit amet quam. Quisque elementum orci non sagittis commodo.<br />
+                                        In egestas ligula turpis, id scelerisque arcu pharetra et.<br />
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /></p></span>
                                   </p>
                                 </div>
-
-
                                 <div className=" px-10">
-                                  <img className="flex rounded-xl justify-center items-center mt-5 mb-3" src="https://img.global.news.samsung.com/br/wp-content/uploads/2018/01/flip-samsung.png" alt="Imagem de exemplo"></img>
+                                  <img className="flex rounded-xl justify-center items-center mt-5 mb-3 " src="https://img.global.news.samsung.com/br/wp-content/uploads/2018/01/flip-samsung.png" alt="Imagem de exemplo"></img>
                                 </div>
-                                
                                 <div className="flex justify-center items-center">
                                   <button onClick={() => leiaMais(taskId)} id={`btnLeiaMais-${taskId}`}>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-500 mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M19 9l-7 7-7-7" /></svg>
                                   </button>
                                 </div>
-
-                              </ul>
-                            </div>
-                          );
-                        })}
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+                              </div>
+                            );
+                          })}
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
