02c695190c6ce19c5531e22081922b128cd72676
felipesobrinho <felipe.sobrinho.3@outlook.com>
2024-06-12 17:04:04 -0300

    criação mobile services

---
+++ src/mobile/src/services/admin.services.js
@@ -0,0 +1,59 @@
+import API from './webapi.services';
+import { BASE_URL } from './urls';
+
+export const getAdmin = async () => {
+  try {
+    const response = await API.get(`${BASE_URL}/admin`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const getAdminProfile = async (id) => {
+  try {
+      const response = await API.get(`${BASE_URL}/admin/profile/${id}`);
+      return response.data;
+  } catch (error) {
+      console.log(error);
+      return null;
+  }
+};
+
+export const createAdmin = async (data) => {
+  try {
+    const response = await API.post(`${BASE_URL}/admin`, data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const updateAdmin = async (id) => {
+  try {
+    const response = await API.patch(`${BASE_URL}/admin`, {
+        id: data._id,
+        name: data.nome,
+        class_number: data.class_number,
+        email: data.email,
+        password: data.senha
+    });;
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+
+export const deleteAdmin = async (id) => {
+  try {
+    const response = await API.delete(`${BASE_URL}/admin/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
---
+++ src/mobile/src/services/professor.services.js
@@ -0,0 +1,59 @@
+import API from './webapi.services';
+import { BASE_URL } from './urls';
+
+export const getTeacher = async () => {
+  try {
+    const response = await API.get(`${BASE_URL}/teacher`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const getTeacherProfile = async (id) => {
+  try {
+      const response = await API.get(`${BASE_URL}/teacher/profile/${id}`);
+      return response.data;
+  } catch (error) {
+      console.log(error);
+      return null;
+  }
+};
+
+export const createTeacher = async (data) => {
+  try {
+    const response = await API.post(`${BASE_URL}/teacher`, data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const updateTeacher = async (id) => {
+  try {
+    const response = await API.patch(`${BASE_URL}/teacher`, {
+        id: data._id,
+        name: data.nome,
+        class_number: data.class_number,
+        email: data.email,
+        password: data.senha
+    });;
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+
+export const deleteTeacher = async (id) => {
+  try {
+    const response = await API.delete(`${BASE_URL}/teacher/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
