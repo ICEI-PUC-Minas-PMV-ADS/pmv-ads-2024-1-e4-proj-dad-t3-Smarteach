2713221c87dc39da719b49d35290c1e127067120
felipesobrinho <felipe.sobrinho.3@outlook.com>
2024-06-17 18:00:04 -0300

    arrumado problema com as requisições API

--- src/Back-end/app/controller/__init__.py
+++ src/Back-end/app/controller/__init__.py
@@ -1,12 +1,13 @@
 import os
-import urllib.parse
+# import urllib.parse
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
-DB_USER = urllib.parse.quote_plus(os.getenv('DB_USER'))
-PASSWORD = urllib.parse.quote_plus(os.getenv('PASSWORD'))
-STR_CONNECTION = os.getenv('DB_STR_CONNECTION').format(DB_USER, PASSWORD)
+# DB_USER = urllib.parse.quote_plus(os.getenv('DB_USER'))
+# PASSWORD = urllib.parse.quote_plus(os.getenv('PASSWORD'))
+
+STR_CONNECTION = os.getenv('DB_STR_CONNECTION')
 
 client = MongoClient(STR_CONNECTION)
 
