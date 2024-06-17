bfee1937af84d3fb5bab51ec3fde7a8b2ce57897
Geovanne <114536934+Geovanneh@users.noreply.github.com>
2024-06-17 20:49:56 -0300

    Upload Tela Configuracoes e adição tela usuários

--- src/mobile/src/screens/Usuarios.js
+++ src/mobile/src/screens/Usuarios.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, Dimensions, } from 'react-native';
-
 import Header from '../components/Header';
 
 export default function Mural() {
@@ -9,21 +8,16 @@ export default function Mural() {
             <Header />
             <View style={styles.container}>
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
-                    <View style={{ height: 300, width: 340, marginTop: 130, backgroundColor: '#004AAD', borderRadius: 10, justifyContent: 'center', gap: 20,}}>
+                    <View style={{ height: 300, width: 340, marginTop: 130, backgroundColor: '#004AAD', borderRadius: 10, justifyContent: 'center', gap: 20, }}>
                         <Text style={{ marginLeft: 10, color: '#ffffff', fontWeight: 500, fontSize: 20, }}>Nome: Geremildo</Text>
                         <Text style={{ marginLeft: 10, color: '#ffffff', fontWeight: 500, fontSize: 20, }}>Tipo de Perfil: Administrador(a)</Text>
                         <Text style={{ marginLeft: 10, color: '#ffffff', fontWeight: 500, fontSize: 20, }}>Email: Administrador@homtmail.com</Text>
                     </View>
-
-
                 </View>
             </View>
-
         </ScrollView>
-
     );
 };
-
 const styles = StyleSheet.create({
     body: {
         flex: 1,
@@ -34,6 +28,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginHorizontal: 16,
         marginVertical: 16,
-        
     },
 });
